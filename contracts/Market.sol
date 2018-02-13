986957c26fbbfe680842367022db5ad51c228e33
Jonathan Araneda <jonathanaraneda@MacBook-Pro-de-Jonathan.local>
2018-02-13 00:13:13 -0300

    first commit

---
+++ contracts/Market.sol
@@ -0,0 +1,12 @@
+pragma solidity ^0.4.4;
+
+contract Market {
+
+  mapping (uint256 => address) public beastIndexToOwner;
+
+  function Market() public {
+    // constructor
+  }
+
+  
+}
---
+++ contracts/Storage.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.4.4;
+
+contract Storage {
+  uint256 storedData;
+function set(uint256 data) public {
+    storedData = data;
+  }
+function get() public constant returns (uint256) {
+    return storedData;
+  }
+}
