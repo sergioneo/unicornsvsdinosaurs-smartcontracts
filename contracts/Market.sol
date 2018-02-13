fbeeb13a4c5fd2b4bb93117fc68ee5e7d6f3fa32
Jonathan Araneda <jonathanaraneda@MacBook-Pro-de-Jonathan.local>
2018-02-13 00:56:59 -0300

    base market and beast

--- contracts/Market.sol
+++ contracts/Market.sol
@@ -2,11 +2,24 @@ pragma solidity ^0.4.4;
 
 contract Market {
 
-  mapping (uint256 => address) public beastIndexToOwner;
-
-  function Market() public {
-    // constructor
-  }
-
+  event BestAcquired(uint256 _beastId);
+  event BestOffered(uint256 _beastId, uint _price);
   
+  mapping (uint256 => address) public beastIndexToOwner;
+  uint256[] bestInAuction;
+  
+  function MarketV2() public {}
+  
+  function offerBeast(address _seller, uint256 _beastId, uint256 _priceWei) public {
+      require( _seller != address(0) );
+      
+  }
+  function acquireBeast(address _buyer, uint256 _beastId) public payable {
+      require( _buyer != address(0) );
+      uint256 weiAmount = msg.value;
+  }
+  
+  function _transferBeast() internal {}
+  
+  function () external payable {}
 }
