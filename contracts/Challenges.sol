49031f6b669e9d9eb900df4d51b4b70b510ef4f2
Nicolas Palacios <magonicolas@gmail.com>
2018-02-19 15:53:45 -0300

    Started Challenges file.

---
+++ contracts/Challenges.sol
@@ -0,0 +1,38 @@
+pragma solidity ^0.4.18;
+
+contract Challenges is AccessControl {
+
+	struct Challenge {
+		uint id;
+		string name;
+		uint strengthPonderation;
+		uint dexterityPonderation;
+		uint endurancePonderation;
+		uint knowledgePonderation;
+		uint wisdomPonderation;
+		uint charismaonderation;
+	}
+
+	mapping (uint => Challenge) public challenges;
+
+	function Challenges () {
+		
+	}	
+
+	function createChallenge(uint _id, string _name, uint _strengthPonderation, uint _dexterityPonderation,
+	 uint _endurancePonderation, uint _knowledgePonderation, uint _wisdomPonderation, uint _charismaonderation) 
+	onlyCOO {
+		Challenge memory _challenge = Challenge({
+			id: _id,
+            name: _name,
+            strengthPonderation: _strengthPonderation,
+            dexterityPonderation: _dexterityPonderation,
+            endurancePonderation: _endurancePonderation,
+            knowledgePonderation: _knowledgePonderation,
+            wisdomPonderation: _wisdomPonderation,
+            charismaonderation: _charismaonderation
+        });
+        challenges[_id] = _challenge;
+	}
+}
+
