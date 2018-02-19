e0ea94745d444e902f17708fbfc15ae89132a568
Nicolas Palacios <magonicolas@gmail.com>
2018-02-19 17:45:31 -0300

    Create Experience System function.

--- contracts/ExperienceSystems.sol
+++ contracts/ExperienceSystems.sol
@@ -4,23 +4,34 @@ import './BeastBase.sol';
 
 contract ExperienceSystems {
 
-	struct Challenge {
+	struct ExperienceSystem {
 		uint id;
-		string name;
-		bool isActive;
-		uint minLevelRequired;
-		uint strengthPonderation;
-		uint dexterityPonderation;
-		uint endurancePonderation;
-		uint knowledgePonderation;
-		uint wisdomPonderation;
-		uint charismaPonderation;
-		uint randomFactor; // How much you can add to the sum calculation.
-		// TODO: This should be an Exp System
-		uint expMultiplicator;
+		uint base;
+		uint percentaje;
+		bool isJustBase;
+		bool isJustPercentaje;
+		uint dinosaurBonus;
+		uint unicornBonus;
 	}
+
+	mapping (uint => ExperienceSystem) public experienceSystems;
 
 	function ExperienceSystems () {
 		
 	}	
+
+	function createExperienceSystem(uint _id, uint _base, uint _percentaje, bool _isJustBase,
+	 bool _isJustPercentaje, uint _dinosaurBonus, uint _unicornBonus)  {
+			ExperienceSystem memory _experienceSystems = ExperienceSystem({
+			id: _id,
+            base: _base,
+            percentaje: _percentaje,
+            isJustBase: _isJustBase,
+            isJustPercentaje: _isJustPercentaje,
+            dinosaurBonus: _dinosaurBonus,
+            unicornBonus: _unicornBonus
+        });
+        experienceSystems[_id] = _experienceSystems;
+	}
+	
 }
