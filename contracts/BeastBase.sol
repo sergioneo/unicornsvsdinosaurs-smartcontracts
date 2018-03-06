1fae388e496eed76aa44418d7806225e009984c5
Nicolas Palacios <magonicolas@gmail.com>
2018-03-06 15:23:07 -0300

    WIP skillBonus + TODOs.

--- contracts/BeastBase.sol
+++ contracts/BeastBase.sol
@@ -45,6 +45,7 @@ contract BeastBase is AccessControl {
         uint32 siringWithId;
         uint16 cooldownIndex;
         uint16 generation;
+        //TODO: Add skillId;
         uint8 level; // the level of the beast, based on experience.
         uint8 preferedAttribute; // The one atttribute that we want to increment when level up.(0,1,2,3,4 or 5)
         Rarity pedigree;
@@ -74,30 +75,6 @@ contract BeastBase is AccessControl {
         uint32(2 days),
         uint32(4 days),
         uint32(7 days)
-    ];
-
-    /// Table indicating experience requiere for each level
-    uint[20] public experienceRequiredForLevel = [
-        uint(2),
-        uint(4),
-        uint(8),
-        uint(16),
-        uint(32),
-        uint(64),
-        uint(128),
-        uint(256),
-        uint(512),
-        uint(1024),
-        uint(2048),
-        uint(4096),
-        uint(8192),
-        uint(16384),
-        uint(32768),
-        uint(65536),
-        uint(131072),
-        uint(262144),
-        uint(524288),
-        uint(1048576),
     ];
 
     // An approximation of currently how many seconds are in between blocks.
