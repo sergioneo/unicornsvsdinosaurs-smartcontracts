50c9bcda53e243c01da10cfdb4f93567cd4fed33
Nicolas Palacios <magonicolas@gmail.com>
2018-03-06 12:27:08 -0300

    experienceRequiredForLevel moved to other file.

--- contracts/BeastAttributes.sol
+++ contracts/BeastAttributes.sol
@@ -13,6 +13,30 @@ contract BeastAttributes is AccessControl {
 
     event ChangePreferedAttribute(uint _beastID, uint _from, uint _to);
 
+    /// Table indicating experience requiere for each level
+    uint[20] public experienceRequiredForLevel = [
+        uint(2),
+        uint(4),
+        uint(8),
+        uint(16),
+        uint(32),
+        uint(64),
+        uint(128),
+        uint(256),
+        uint(512),
+        uint(1024),
+        uint(2048),
+        uint(4096),
+        uint(8192),
+        uint(16384),
+        uint(32768),
+        uint(65536),
+        uint(131072),
+        uint(262144),
+        uint(524288),
+        uint(1048576),
+    ];
+
 
     function levelUp(uint _tokenId, uint _toLevel) internal {
         Beast storage _beast = beasts[_tokenId]
