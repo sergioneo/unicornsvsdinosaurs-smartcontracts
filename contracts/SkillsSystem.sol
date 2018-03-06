3f320746e6dd606cbc41e7dbf6bd3485c926e17a
Nicolas Palacios <magonicolas@gmail.com>
2018-03-06 10:23:32 -0300

    Variables added to Skil struct.

--- contracts/SkillsSystem.sol
+++ contracts/SkillsSystem.sol
@@ -5,6 +5,18 @@ contract SkillsSystem {
 	struct Skill {
 		uint id;
 		string name;
+		uint attirbuteId;
+		bool addAttributePercentaje;
+		uint winExperienceBonus;
+		uint loseExperienceBonus;
+		uint elementalAdvantageBonus;
+		uint elementalDisadvantageBonus;
+		uint randomAttributeBonus;
+		bool has5LevelBonus;
+		bool preferedAttributeIncreasedChance;
+		bool fertileSpeedBonus;
+		bool dominantGene;
+		bool recesiveGene;
 	}
 
 	mapping (uint => Challenge) public skills;
