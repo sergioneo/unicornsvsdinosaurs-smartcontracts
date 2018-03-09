58317385a420345559b4343a038f024feef8b1a5
Jonathan Araneda <jaraneda88@gmail.com>
2018-03-09 01:13:37 -0300

    remove compiled files from repo

--- migrations/4_dev_legend.js
+++
@@ -1,8 +0,0 @@
-var Legends = artifacts.require("./Legends.sol");
-var SaleClockAuction = artifacts.require("./auction/SaleClockAuction.sol");
-
-module.exports = function(deployer, network, accounts) {
-  deployer.deploy(Legends).then(function(){
-    return deployer.deploy( SaleClockAuction, Legends.address, 9000 );
-  });
-};
