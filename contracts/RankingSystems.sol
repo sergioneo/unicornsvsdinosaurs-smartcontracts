04df7a6579970894a3da4fcfa819442f07c178ec
Jonathan Araneda <jaraneda88@gmail.com>
2018-03-12 01:53:50 -0300

    refactor of ranking system

--- contracts/RankingSystems.sol
+++ contracts/RankingSystems.sol
@@ -2,42 +2,80 @@ pragma solidity ^0.4.18;
 
 import './BeastBase.sol';
 
-contract RankingSystems {
+contract RankingSystems is BeastBase {
 
-	struct RankingSystem {
-		uint id;
-		uint[10] top10Ids = [0,0,0,0,0,0,0,0,0,0]
-		uint top10MinWins;
-		uint[10] top10DinosaursIds = [0,0,0,0,0,0,0,0,0,0]
-		uint top10DinosaurMinWins;
-		uint[10] top10UnicornsIds = [0,0,0,0,0,0,0,0,0,0]
-		uint top10UnicornMinWins;
-	}
+  uint private constant TOP_RANK_PER_GAME = 10;
 
-	mapping (uint => RankingSystem) public rankingSystems;
+  struct RankingStruct {
+    mapping (uint256 => uint256) rankingCount; 
+    uint256[] rankingList;
+    uint256 minToRank;
+    bool isRanking;
+  } 
 
-	function RankingSystems () {
-		
-	}
+  // @dev: uint256 is the ID of the game
+  mapping (uint256 => RankingStruct) public rankings;
+  mapping (uint8 => uint256) public globalRank; // races rank, index will be the bits, as int, of the race
 
-	function handleTop10(uint _rankId, uint _beastId, uint _wins) internal {
-		RankingSystem memory _rankSystem= rankingSystems[_rankId];
-		
-		require(_rankSystem.top10MinWins < top10MinWins || _rankSystem.top10MinWins < top10DinosaurMinWins || 
-			_rankSystem.top10MinWins < top10UnicornMinWins);
-		// TODO: Check if dino or UNI and add to correspondig ranking.
-		if(_wins > _rankSystem.top10MinWins) {
-			addToTop10Global(_rankId, _beastId, _wins);
-		}
-	}
+  function rankingExists( uint256 rankingId) internal returns(bool) {
+    return rankings[rankingId].isRanking;
+  }
 
-	function addToTop10Global(uint _rankId, uint _beastId, uint _wins) internal {
-		RankingSystem memory _rankSystem= rankingSystems[_rankId];
-		// TODO: Handle adding to top 10 global.
-		// for(uint i = 0; i < 10; i++) {
-		// 	if (_wins > _rankSystem.top10Ids[i]) {
+  // reset all ranks
+  function resetLeaderboards() public onlyCEO {
+    // TODO: reset all storages
+	// @dev: delete of mapping will be the hardest thing to do
+  }  
 
-		// 	}
-  //      	}
-	}
+  // TODO: refactor
+  // @dev: keep eye on GAS !!
+  function registerWinner( uint256 gameId, uint256 beastId ) internal {
+
+    if (!rankingExists(gameId)) { // create the ranking
+      rankings[gameId].isRanking = true;
+    }
+
+    rankings[gameId].rankingCount[beastId] += 1; // add a winning to the beast
+    // TODO: get race to add to global rank
+
+    if (rankings[gameId].rankingList.length < TOP_RANK_PER_GAME) { // easy add
+      if ( !_findInArray(rankings[gameId].rankingList, beastId) ) {
+        rankings[gameId].rankingList.push(beastId);
+      }
+      
+      if ( rankings[gameId].rankingList.length == 1 || rankings[gameId].rankingCount[beastId] < rankings[gameId].minToRank ) {
+        rankings[gameId].minToRank = rankings[gameId].rankingCount[beastId];
+      }
+
+    } else { // hard add
+      if ( _findInArray(rankings[gameId].rankingList, beastId) ) {
+        if ( rankings[gameId].rankingList.length == 1 || rankings[gameId].rankingCount[beastId] < rankings[gameId].minToRank ) {
+          rankings[gameId].minToRank = rankings[gameId].rankingCount[beastId];
+        }
+      } else { // we need remove a value from the list
+
+      }
+    }
+  }
+
+  // @dev: web must convert to ascii the byte32 array
+  function getRanking( uint256 gameId ) public view returns(bytes32[] beastList) {
+    
+    uint256[] rankingList = rankings[gameId].rankingList;
+    mapping (uint256 => uint256) rankingCount = rankings[gameId].rankingCount;
+
+    for (uint i = 0; i < rankingList.length; i++) {
+
+    }
+
+  }
+
+  function _findInArray( uint256[] arrayOfValues, uint256 valueToFind ) private returns(bool) {
+    for (uint i = 0; i < arrayOfValues.length; i++) {
+      if (arrayOfValues[i] == valueToFind) {
+        return true;
+      }
+    }
+    return false;
+  }
 }
