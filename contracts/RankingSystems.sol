47242597cadef415b0422c0a5fe9e90c073d6cee
Jonathan Araneda <jaraneda88@gmail.com>
2018-03-16 02:52:15 -0300

    registerWinner finished

--- contracts/RankingSystems.sol
+++ contracts/RankingSystems.sol
@@ -4,11 +4,11 @@ import './BeastBase.sol';
 
 contract RankingSystems is BeastBase {
 
-  uint private constant TOP_RANK_PER_GAME = 10;
+  uint256 private constant TOP_RANK_PER_GAME = 10;
 
   struct RankingStruct {
-    mapping (uint256 => uint256) rankingCount; 
-    uint256[] rankingList;
+    mapping (uint256 => uint256) rankingCount; // beastId => winnings
+    uint256[TOP_RANK_PER_GAME] rankingList; // beastIds
     uint256 minToRank;
     bool isRanking;
   } 
@@ -17,20 +17,14 @@ contract RankingSystems is BeastBase {
   mapping (uint256 => RankingStruct) public rankings;
   mapping (uint8 => uint256) public globalRank; // races rank, index will be the bits, as int, of the race
 
-  function rankingExists( uint256 rankingId) internal returns(bool) {
+  function rankingExists( uint256 rankingId) internal view returns(bool) {
     return rankings[rankingId].isRanking;
   }
 
-  // reset all ranks
-  function resetLeaderboards() public onlyCEO {
-    // TODO: reset all storages
-	// @dev: delete of mapping will be the hardest thing to do
-  }  
-
   // TODO: refactor
   // @dev: keep eye on GAS !!
-  function registerWinner( uint256 gameId, uint256 beastId ) internal {
-
+  function registerWinner( uint256 gameId, uint256 beastId ) public {
+    
     if (!rankingExists(gameId)) { // create the ranking
       rankings[gameId].isRanking = true;
     }
@@ -38,39 +32,50 @@ contract RankingSystems is BeastBase {
     rankings[gameId].rankingCount[beastId] += 1; // add a winning to the beast
     // TODO: get race to add to global rank
 
+    _sort(gameId); // sort array based on amount of winnigs
+
     if (rankings[gameId].rankingList.length < TOP_RANK_PER_GAME) { // easy add
       if ( !_findInArray(rankings[gameId].rankingList, beastId) ) {
-        rankings[gameId].rankingList.push(beastId);
+        rankings[gameId].rankingList[rankings[gameId].rankingList.length] = beastId;
       }
       
       if ( rankings[gameId].rankingList.length == 1 || rankings[gameId].rankingCount[beastId] < rankings[gameId].minToRank ) {
         rankings[gameId].minToRank = rankings[gameId].rankingCount[beastId];
       }
 
-    } else { // hard add
+    } else { // dificult add
       if ( _findInArray(rankings[gameId].rankingList, beastId) ) {
         if ( rankings[gameId].rankingList.length == 1 || rankings[gameId].rankingCount[beastId] < rankings[gameId].minToRank ) {
           rankings[gameId].minToRank = rankings[gameId].rankingCount[beastId];
         }
-      } else { // we need remove a value from the list
-
+      } else {
+        // if greater than minimum rank then add to list
+        if ( rankings[gameId].rankingCount[beastId] > rankings[gameId].minToRank ) {
+            // add to list, sort and remove the last
+            //rankings[gameId].rankingList[ rankings[gameId].rankingList.length - 1 ] = beastId;
+            rankings[gameId].rankingList[0] = beastId;
+            rankings[gameId].minToRank = rankings[gameId].rankingCount[beastId];
+        }
+        
       }
     }
   }
 
   // @dev: web must convert to ascii the byte32 array
-  function getRanking( uint256 gameId ) public view returns(bytes32[] beastList) {
+  function getRanking( uint256 gameId ) public view returns(bytes32[TOP_RANK_PER_GAME]) {
     
-    uint256[] rankingList = rankings[gameId].rankingList;
-    mapping (uint256 => uint256) rankingCount = rankings[gameId].rankingCount;
+    bytes32[TOP_RANK_PER_GAME] memory beastList;
+    uint256[TOP_RANK_PER_GAME] memory rankingList = rankings[gameId].rankingList;
 
     for (uint i = 0; i < rankingList.length; i++) {
-
+        bytes32 rankingMember = bytes32(rankingList[i]);
+        beastList[i] = rankingMember;
     }
-
+    
+    return beastList;
   }
 
-  function _findInArray( uint256[] arrayOfValues, uint256 valueToFind ) private returns(bool) {
+  function _findInArray( uint256[TOP_RANK_PER_GAME] arrayOfValues, uint256 valueToFind ) private pure returns(bool) {
     for (uint i = 0; i < arrayOfValues.length; i++) {
       if (arrayOfValues[i] == valueToFind) {
         return true;
@@ -78,4 +83,31 @@ contract RankingSystems is BeastBase {
     }
     return false;
   }
+  
+    // @dev ref from  https://github.com/alianse777/solidity-standard-library/blob/master/Array.sol
+    // must be here because the array ref
+    function _sortItem(uint256 gameId, uint pos) private returns (bool) {
+        uint w_min = pos;
+        for(uint i = pos;i < rankings[gameId].rankingList.length;i++) {
+            if( rankings[gameId].rankingCount[rankings[gameId].rankingList[i]] < rankings[gameId].rankingCount[rankings[gameId].rankingList[w_min]]) {
+                w_min = i;
+            }
+        }
+        if(w_min == pos) {
+          return false;
+        }
+        uint tmp = rankings[gameId].rankingList[pos];
+        rankings[gameId].rankingList[pos] = rankings[gameId].rankingList[w_min];
+        rankings[gameId].rankingList[w_min] = tmp;
+        return true;
+    }
+    
+    /**
+     * @dev Sort the array
+     */
+    function _sort( uint256 gameId ) private {
+        for( uint i = 0; i < rankings[gameId].rankingList.length-1; i++ ) {
+            _sortItem(gameId, i);
+        }
+    }
 }
