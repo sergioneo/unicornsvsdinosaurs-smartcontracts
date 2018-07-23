ac3f254d21e5f8853803648382514cf797d46355
Araneda Labarca <j.araneda.labarca@accenture.com>
2018-07-23 18:10:36 -0400

    dummy random

--- contracts/util/Random.sol
+++ contracts/util/Random.sol
@@ -29,7 +29,18 @@ contract Random {
 
     // return a pseudo random number between lower and upper bounds
     // given the number of previous blocks it should hash.
-    function random(uint256 upper) internal returns (uint256 randomNumber) {
-        return maxRandom() % upper;
+    function random(uint256 upper) internal view returns (uint256 randomNumber) {
+        //return maxRandom() % upper;
+        uint256[8] memory arr;
+        arr[0] = uint256(297491009538120220672);
+        arr[1] = uint256(297492135438027063296);
+        arr[2] = uint256(297493261337933905920);
+        arr[3] = uint256(297494387237840748544);
+        arr[4] = uint256(470662549096890368);
+        arr[5] = uint256(470663648608518144);
+        //arr[6] = uint256(470664748120145920);
+        //arr[7] = uint256(470665847631773696);
+        
+        return arr[uint256(keccak256(block.timestamp))%5 +1];
     }
 }
