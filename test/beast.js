34621cf0feeeeb2839775e3a422ecce41c607ece
Jonathan Araneda <jaraneda88@gmail.com>
2018-08-05 08:14:47 -0400

    working in test

---
+++ test/beast.js
@@ -0,0 +1,9 @@
+var Beasts = artifacts.require("./Beasts.sol");
+var SaleClockAuction = artifacts.require("./auction/SaleClockAuction.sol");
+var SiringClockAuction = artifacts.require("./auction/SiringClockAuction.sol");
+var GeneMagic = artifacts.require("./GeneMagic.sol");
+
+contract('Beast', async (accounts) => {
+    it("")
+
+})
