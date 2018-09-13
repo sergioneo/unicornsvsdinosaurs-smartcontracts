226b978164c7af5c23ab38b1f9a70ef3d4a9545c
Jonathan Araneda <jaraneda88@gmail.com>
2018-09-13 01:51:01 -0300

    add new function

--- contracts/interface/EggFactoryInterface.sol
+++ contracts/interface/EggFactoryInterface.sol
@@ -2,5 +2,5 @@ pragma solidity ^0.4.24;
 
 contract EggFactoryInterface {
     function isEggFactory() public pure returns (bool);
-    function openEgg(uint256 _eggId, uint256 _amount) external;
+    function eggsOwned(address, uint256) public returns (uint256);
 }
