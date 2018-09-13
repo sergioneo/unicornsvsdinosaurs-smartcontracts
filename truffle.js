02f395dbee005df6c9c03b359999932e0bf04afe
Jonathan Araneda <jaraneda88@gmail.com>
2018-09-13 01:52:00 -0300

    adjust ganache gas

--- truffle.js
+++ truffle.js
@@ -15,8 +15,8 @@ module.exports = {
       host: "127.0.0.1",
       port: 7545,
       network_id: "5777",
-      gas: 6721975,
-      gasPrice: 20000000000
+      gas: 67219750,
+      gasPrice: 2000000000
     },
     rinkeby: {
       host: "localhost", // Connect to geth on the specified
